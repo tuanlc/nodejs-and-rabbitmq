44115c917e8532464dc0c90c7cfeb8eb3daaee0c
Tuan LE CONG <tuancnttbk93@gmail.com>
2023-05-27 16:53:00 +0700

    Node app

---
+++ app/src/config/index.ts
@@ -0,0 +1,4 @@
+export const RABBITMQ_USER = process.env.RABBITMQ_USER || 'admin';
+export const RABBITMQ_PASS = process.env.RABBITMQ_PASS || 'admin123';
+export const RABBITMQ_HOST = process.env.RABBITMQ_HOST || 'localhost';
+export const RABBITMQ_PORT = process.env.RABBITMQ_PORT || '5672';
